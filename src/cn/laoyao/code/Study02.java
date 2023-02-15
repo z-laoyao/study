5c247308a3d2afdcb6384774999a2cc5184566d3
laoyao <laoyao777@outlook.com>
2023-02-15 13:30:54 +0800

    第三次提交

--- src/cn/laoyao/code/Study02.java
+++ src/cn/laoyao/code/Study02.java
@@ -3,5 +3,7 @@ package cn.laoyao.code;
 public class Study02 {
     public static void main(String[] args) {
         System.out.println("helo!!");
+        System.out.println("kasjdfajsd");
+        System.out.println("ddddddddddddddd");
     }
 }
