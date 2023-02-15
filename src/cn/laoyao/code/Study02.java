206571a4dd618c326cf9b4fa9c82e2b6897cb123
laoyao <laoyao777@outlook.com>
2023-02-15 14:43:09 +0800

    第6次提交

--- src/cn/laoyao/code/Study02.java
+++ src/cn/laoyao/code/Study02.java
@@ -5,5 +5,6 @@ public class Study02 {
         System.out.println("helo!!");
         System.out.println("kasjdfajsd");
         System.out.println("dddd22222222d");
+        System.out.println(1221);
     }
 }
